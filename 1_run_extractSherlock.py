d173ea2f1293b3466b3d742608d007cac704147a
Vanessa Sochat <vsochat@stanford.edu>
2018-12-14 02:33:58 -0500

    buggers! :bug:

--- 1_run_extractSherlock.py
+++ 1_run_extractSherlock.py
@@ -2,6 +2,7 @@
 
 import os
 import pickle
+import time
 
 # Make directory for working
 # module load python/3.6.1
@@ -23,21 +24,40 @@ for dirname in ['.job', '.out', output_dir]:
 # cache directory for layers
 cache_dir = "/scratch/users/vsochat/.singularity/docker"
 
+# Function to ensure we don't go over job limit (1000)
+def count_queue():
+    user = os.environ['USER']
+    return int(os.popen('squeue -u %s | wc -l' %user).read().strip('\n'))
+
+
+# Step 2. Generate jobs, add any that don't get to run to list
+jobs = []
+job_limit = 1000
+
+count = count_queue()
+
 # For each container, run a container-diff job
 for name in names:
     print("Processing %s" % name)
     filename = name.replace('/','-')
     file_name = ".job/%s.job" %(filename)
     output_json = os.path.join(output_dir, '%s.json' % filename)
-    with open(file_name, "w") as filey:
-        filey.writelines("#!/bin/bash\n")
-        filey.writelines("#SBATCH --job-name=%s\n" %filename)
-        filey.writelines("#SBATCH --output=.out/%s.out\n" %filename)
-        filey.writelines("#SBATCH --error=.out/%s.err\n" %filename)
-        filey.writelines("#SBATCH --time=60:00\n")
-        filey.writelines("#SBATCH --mem=8000\n")
-        filey.writelines('module load python/3.6.1')
-        filey.writelines("/bin/bash 1_extractSherlock.sh %s %s %s\n" % (output_json, name, cache_dir))
-        os.system("sbatch -p owners .job/%s.job" %filename)
+    if not os.path.exists(output_json):
+        if count < job_limit:
+            with open(file_name, "w") as filey:
+                filey.writelines("#!/bin/bash\n")
+                filey.writelines("#SBATCH --job-name=%s\n" %filename)
+                filey.writelines("#SBATCH --output=.out/%s.out\n" %filename)
+                filey.writelines("#SBATCH --error=.out/%s.err\n" %filename)
+                filey.writelines("#SBATCH --time=30:00\n")
+                filey.writelines("#SBATCH --mem=8000\n")
+                filey.writelines('module load python/3.6.1\n')
+                filey.writelines("/bin/bash 1_extractSherlock.sh %s %s %s\n" % (output_json, name, cache_dir))
+                filey.writelines("rm .job/%s.job\n" % os.path.abspath(filename))
+
+            os.system("sbatch -p owners .job/%s.job" %filename)
+        else:
+            jobs.append(file_name)
+            time.sleep(1)
 
 # /bin/bash 1_extractSherlock.sh /scratch/users/vsochat/WORK/dockerfiles/container-diff/alerta-alerta-web.json alerta/alerta-web /scratch/users/vsochat/.singularity/docker
